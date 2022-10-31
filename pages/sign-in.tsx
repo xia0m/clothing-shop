e850efc1eafad9de54adc8bef34f7ff17ac90c32
xia0m <alexma325@gmail.com>
2022-10-30 23:55:11 -0700

    added sign-in page skelton

---
+++ pages/sign-in.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
+import Shop from './shop';
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>Sign In Page</div>
+    </Layout>
+  );
+}
