3a579ddf59d5682013c14df9ee6cdfcbb3f47c6f
xia0m <alexma325@gmail.com>
2022-10-30 23:44:20 -0700

    added navigation control bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,12 +4,24 @@ import { Fragment } from 'react';
 export default function NavBar() {
   return (
     <Fragment>
-      <div>
-        <Link href="/">Logo</Link>
-      </div>
-      <div>
-        <Link href="/shop">Shop</Link>
-        <Link href="/sign-in">SIGN IN</Link>
+      <div className="h-[70px] w-full flex justify-between mb-[25px]">
+        <Link href="/" className="h-full w-[70px] p-[25px]">
+          Logo
+        </Link>
+        <div className="w-1/2 h-full flex items-center justify-end">
+          <Link
+            className="px-[10px] py-[15px] cursor-pointer uppercase"
+            href="/shop"
+          >
+            Shop
+          </Link>
+          <Link
+            className="px-[10px] py-[15px] cursor-pointer uppercase"
+            href="/sign-in"
+          >
+            Sign In
+          </Link>
+        </div>
       </div>
     </Fragment>
   );
