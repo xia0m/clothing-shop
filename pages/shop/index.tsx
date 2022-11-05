5d8ba1d3ba40c88f87ca04f848a145c016229430
xia0m <alexma325@gmail.com>
2022-11-05 00:09:59 -0700

    added shop page style

--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -4,7 +4,7 @@ import ProductCard from '../../components/ProductCard';
 export default function Shop() {
   return (
     <div>
-      <div className="w-full flex flex-wrap justify-between">
+      <div className="grid grid-cols-4 gap-[10px] gap-y-[50px]">
         {PRODUCTS.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
