968d22533735d6d7c407642fc868ddc4cc720590
xia0m <alexma325@gmail.com>
2022-11-11 00:03:26 -0800

    added navigation bar in shop page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
 export default function NavBar() {
-  const router = useRouter();
   return (
     <Fragment>
       <div className="h-[70px] w-full flex justify-between mb-[25px]">
-        <Link href="/" className="h-full w-[70px] p-[25px]">
+        <Link href="/" className="h-full w-[70px] p-[15px]">
           <Image src="/crown.svg" alt="logo" height={50} width={50} />
         </Link>
         <div className="w-1/2 h-full flex items-center justify-end">
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -1,14 +1,15 @@
 import PRODUCTS from '../../assets/shop-data.json';
+import Layout from '../../components/Layout';
 import ProductCard from '../../components/ProductCard';
 
 export default function Shop() {
   return (
-    <div>
+    <Layout>
       <div className="grid grid-cols-4 gap-[10px] gap-y-[50px]">
         {PRODUCTS.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-    </div>
+    </Layout>
   );
 }
