1cf0fac4bd717f529c6e56f5318423895b1e5694
xia0m <alexma325@gmail.com>
2022-11-10 23:55:25 -0800

    added logo link

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,15 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <Fragment>
       <div className="h-[70px] w-full flex justify-between mb-[25px]">
         <Link href="/" className="h-full w-[70px] p-[25px]">
-          Logo
+          <Image src="/crown.svg" alt="logo" height={50} width={50} />
         </Link>
         <div className="w-1/2 h-full flex items-center justify-end">
           <Link
@@ -17,7 +20,7 @@ export default function NavBar() {
           </Link>
           <Link
             className="px-[10px] py-[15px] cursor-pointer uppercase"
-            href="/sign-in"
+            href="/auth"
           >
             Sign In
           </Link>
