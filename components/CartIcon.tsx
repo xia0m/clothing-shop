822a144d9409e810d4ecef6d2bed074018be6132
xia0m <alexma325@gmail.com>
2022-11-23 23:40:07 -0800

    updated cart icon

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -4,6 +4,12 @@ import { useCartStore } from '../store/cartStore';
 export default function CartIcon() {
   const isCartOpen = useCartStore((store) => store.isCartOpen);
   const setIsCartOpen = useCartStore((store) => store.setIsCartOpen);
+  const cartItems = useCartStore((store) => store.cartItems);
+
+  const cartTotal = cartItems.reduce(
+    (accu, currentValue) => accu + currentValue.quantity,
+    0
+  );
 
   const toggleCartDropdown = () => {
     setIsCartOpen(!isCartOpen);
@@ -15,7 +21,9 @@ export default function CartIcon() {
       onClick={toggleCartDropdown}
     >
       <Image src="/shopping-bag.svg" alt="logo" height={24} width={24} />
-      <span className="absolute text-[10px] font-bold bottom-[12px]">0</span>
+      <span className="absolute text-[10px] font-bold bottom-[12px]">
+        {cartTotal}
+      </span>
     </div>
   );
 }
