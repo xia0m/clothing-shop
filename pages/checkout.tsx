ccc092611a39eeb0373fdeb67da40ab1bf7e6298
xia0m <alexma325@gmail.com>
2022-11-25 23:30:41 -0800

    added checkout skeleton

---
+++ pages/checkout.tsx
@@ -0,0 +1,29 @@
+import Layout from '../components/Layout';
+
+export default function Checkout() {
+  return (
+    <Layout>
+      <div>
+        <div>
+          <div>
+            <span>Product</span>
+          </div>
+          <div>
+            <span>Description</span>
+          </div>
+          <div>
+            <span>Quantity</span>
+          </div>
+          <div>
+            <span>Price</span>
+          </div>
+          <div>
+            <span>Remove</span>
+          </div>
+          <div>Item</div>
+          <div>Total: $100</div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
