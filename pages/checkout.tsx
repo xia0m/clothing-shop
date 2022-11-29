2c6ab62d0dde8539321fad8de9e6829138717f7e
xia0m <alexma325@gmail.com>
2022-11-28 23:44:14 -0800

    added style for checkout page header

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,21 +3,21 @@ import Layout from '../components/Layout';
 export default function Checkout() {
   return (
     <Layout>
-      <div>
-        <div>
-          <div>
+      <div className="w-[55%] min-h-[90vh] flex flex-col items-center mt-[50px] mx-auto mb-0">
+        <div className="w-full py-[10px] px-0 flex justify-between border-b border-b-[darygrey] border-solid">
+          <div className="capitalize w-[23%]">
             <span>Product</span>
           </div>
-          <div>
+          <div className="capitalize w-[23%]">
             <span>Description</span>
           </div>
-          <div>
+          <div className="capitalize w-[23%]">
             <span>Quantity</span>
           </div>
-          <div>
+          <div className="capitalize w-[23%]">
             <span>Price</span>
           </div>
-          <div>
+          <div className="capitalize w-[23%]">
             <span>Remove</span>
           </div>
           <div>Item</div>
