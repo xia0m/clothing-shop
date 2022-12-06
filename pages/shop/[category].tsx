686dc1f44a33f1633dbaad6d52189c6578cdc6c7
xia0m <alexma325@gmail.com>
2022-12-05 23:50:42 -0800

    modified category page

--- pages/shop/[category].tsx
+++ pages/shop/[category].tsx
@@ -1,17 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import Layout from '../../components/Layout';
+import PRODUCTS from '../../assets/shop-data.json';
+import ProductCard from '../../components/ProductCard';
 
 type AppProps = {
   title: string;
-}
+};
 
 export default function Category() {
   const router = useRouter();
-  const {category} = router.query;
+  const { category = '' } = router.query;
   return (
-    <div className='text-3xl font-bold underline'>
-      {category}
-    </div>
-  )
+    <Layout>
+      <h2 className="text-3xl mb-[25px] text-center uppercase">{category}</h2>
+      <div className="grid grid-cols-4 gap-x-[50px] gap-y-[20px]">
+        {PRODUCTS.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
